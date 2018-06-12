2cd21e026fc2382784abd6faf6b547d01fc30864
jason <270847975@qq.com>
2018-06-12 11:24:24 +0800

    add admin ui

--- base-serv/admin-server/src/main/java/com/simplemall/micro/serv/base/admin/AdminServerApplicaton.java
+++ base-serv/admin-server/src/main/java/com/simplemall/micro/serv/base/admin/AdminServerApplicaton.java
@@ -7,45 +7,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
 /**
  * 入口方法
  * 
  * @author guooo
  *
  */
-@SpringBootApplication
 @Configuration
-@EnableAutoConfiguration
-@EnableAdminServer
+@EnableAdminServer //开启Spring Boot Admin 服务
+@SpringBootApplication
+@EnableDiscoveryClient
 public class AdminServerApplicaton {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AdminServerApplicaton.class, args);
 	}
 
-	@Configuration
-	public static class SecurityConfig extends WebSecurityConfigurerAdapter {
-		@Override
-		protected void configure(HttpSecurity http) throws Exception {
-			// Page with login form is served as /login.html and does a POST on /login
-			http.formLogin().loginPage("/login.html").loginProcessingUrl("/login").permitAll();
-			// The UI does a POST on /logout on logout
-			http.logout().logoutUrl("/logout");
-			// The ui currently doesn't support csrf
-			http.csrf().disable();
 
-			// Requests for the login page and the static assets are allowed
-			http.authorizeRequests()
-					.antMatchers("/login.html", "/**/*.css", "/img/**", "/third-party/**")
-					.permitAll();
-			// ... and any other request needs to be authorized
-			http.authorizeRequests().antMatchers("/**").authenticated();
-
-			// Enable so that the clients can authenticate via HTTP basic for registering
-			http.httpBasic();
-		}
-	}
 }
